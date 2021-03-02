123ede713885a358aad9b949bd500ea0f5ef30b9
Boris Knezevic <boris.knezevic0@gmail.com>
2021-03-02 18:08:56 +0100

    Added booking create form to space show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,25 @@
 class BookingsController < ApplicationController
+  before_action :set_space, only: :create
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.space = @space
+    @booking.user = current_user
+    @booking.item_type = ItemType.last
+    if @booking.save
+      redirect_to @space
+    else
+      render "spaces/show"
+    end
+  end
+
+  private
+
+  def set_space
+    @space = Space.find(params[:space_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:item_description, :comment, :date)
+  end
 end

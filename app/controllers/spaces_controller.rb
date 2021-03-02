6412f15801531b28b200f93375ff6eaea8726b93
Chris Geekie <chris.geekie@gmail.com>
2021-03-02 17:44:25 +0100

    Added new view for spaces

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -8,7 +8,26 @@ class SpacesController < ApplicationController
   def show
   end
 
+  def new
+    @space = Space.new
+  end
+
+  def create
+    @space = Space.new(space_params)
+    @space.user = current_user
+    @space.item_type = ItemType.all.last
+    if @space.save
+      redirect_to space_path(@space)
+    else
+      render 'new'
+    end
+  end
+
   private
+
+  def space_params
+    params.require(:space).permit(:name, :address, :description, :availability, :phone_number)
+  end
 
   def set_space
     @space = Space.find(params[:id])

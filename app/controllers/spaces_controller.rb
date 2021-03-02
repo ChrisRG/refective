49e3ff1c2e09157e300b53c30e8c5afd84ee1954
Boris Knezevic <boris.knezevic0@gmail.com>
2021-03-02 12:16:56 +0100

    Added spaces show and view

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,2 +1,12 @@
 class SpacesController < ApplicationController
+  before_action :set_space
+  
+  def show
+  end
+
+  private
+
+  def set_space
+    @space = Space.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_153301) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "full_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

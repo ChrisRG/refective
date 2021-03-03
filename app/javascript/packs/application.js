772decdc06c48b68768aff40c59361eec3c1a920
Amara Cole <amara.cole@yahoo.com>
2021-03-03 17:38:41 +0100

    add flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,12 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initFlatpickr } from "../plugins/flatpickr";
+
 
 document.addEventListener('turbolinks:load', () => {
+  initFlatpickr();
   // Call your functions here, e.g:
   // initSelect2();
+
 });

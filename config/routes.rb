d12f7d98e87bd022143e160557f4cc1199243ede
Chris Geekie <chris.geekie@gmail.com>
2021-03-04 16:22:33 +0100

    Added edit modal to booking card in dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :spaces, only: [:index, :show, :new, :create, :destroy] do
     resources :bookings, only: [:create]
   end
-  resources :bookings, only: [:destroy]
+  resources :bookings, only: [:update, :destroy]
 end

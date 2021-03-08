90e3b280fb6475768495c84b74f20fd32c6fba6b
Chris Geekie <chris.geekie@gmail.com>
2021-03-08 11:11:42 +0100

    Added policies for spaces

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,8 +1,8 @@
 class SpacesController < ApplicationController
   before_action :set_space, only: [:show, :destroy, :update]
-  
+   
   def index
-    @spaces = Space.all
+    @spaces = policy_scope(Space)
 
     @markers = @spaces.geocoded.map do |space|
       {
@@ -13,16 +13,17 @@ class SpacesController < ApplicationController
   end
   
   def show
-    @booking = Booking.new
     @markers = [{ lat: @space.latitude, lng: @space.longitude}]
   end
 
   def new
     @space = Space.new
+    authorize @space
   end
 
   def create
     @space = Space.new(space_params)
+    authorize @space
     @space.user = current_user
     # TODO: Add Item Type field to form: f.association :item_Type
     space_item = SpaceItemType.new
@@ -37,13 +38,16 @@ class SpacesController < ApplicationController
   end
 
   def update
-    @space.update(space_params)
-    redirect_to dashboard_path, notice: "Space saved"
+    if @space.update(space_params)
+      redirect_to dashboard_path, notice: "Space saved"
+    else
+      redirect_to dashboard_path, alert: "There was a problem modifying your space."
+    end
   end
 
   def destroy
     @space.destroy
-    redirect_to dashboard_path
+    redirect_to dashboard_path, "Space was successfully deleted."
   end
 
   private
@@ -54,5 +58,6 @@ class SpacesController < ApplicationController
 
   def set_space
     @space = Space.find(params[:id])
+    authorize @space
   end 
 end

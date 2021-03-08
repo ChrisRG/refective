79dcce0a0b79eb9c7411ce39a078aed7badf14c9
Boris Knezevic <boris.knezevic0@gmail.com>
2021-03-08 17:20:10 +0100

    Item filter with JS

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,7 +2,8 @@ class Space < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :space_item_types, dependent: :destroy
-  has_many :reviews, through: :bookings
+  has_many :item_types, through: :space_item_types
+  has_many :reviews, -> { distinct }, through: :bookings
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?

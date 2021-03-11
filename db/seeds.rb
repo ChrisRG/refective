589b4df78dba5e1fc870097e423652b1183f5ecd
Chris Geekie <chris.geekie@gmail.com>
2021-03-11 14:56:00 +0100

    Reworked seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,7 @@ puts "** Seeding users... **"
 user_boris = User.create!(email: "boris@gmail.com", password: "password", full_name: "Boris")
 user_amara = User.create!(email: "amara@gmail.com", password: "password", full_name: "Amara")
 user_chris = User.create!(email: "chris@gmail.com", password: "password", full_name: "Chris")
+owners = [user_boris, user_amara]
 puts "** Users created. **"
 
 # Item Types
@@ -39,123 +40,107 @@ user_list = User.all.to_a
 space1 = Space.new(
   name: "Reparatur-Café Mohr-Villa", 
   address: "Situlistraße 73, 80939 München",
-  description: Faker::Company.catch_phrase, 
+  description: "We're a woodworking shop next the Mohr-Villa community center with a wide selection of hand and power tools. We can also help fix up a lot of other stuff too. Stop on by!",
   availability: GEN_AVAIL_MSG, 
   phone_number: "089 324 32 64",
 )
-space1.user = User.all.to_a.sample
+space1.user = owners.sample
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Furniture").first
+space_item.space = space1
+space_item.save!
 space1.save!
-
 
 space2 = Space.new(
   name: "Repair Café Ottobrunn", 
   address: "Haidgraben 1a, 85521 Ottobrunn", 
-  description: Faker::Company.catch_phrase, 
+  description: "Hello! We're your one stop repair shop for electronics, gadgets, and more! We've got a ton of knowledgeable people to show you around and help you fix your things!",
   availability: GEN_AVAIL_MSG, 
   phone_number: Faker::PhoneNumber.cell_phone_in_e164,
 )
-space2.user = User.all.to_a.sample
+space2.user = owners.sample
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Electronics").first
+space_item.space = space2
+space_item.save
 space2.save
 
 space3 = Space.new(
   name: "Reparaturcafé am Westpark", 
   address: "Ehrwalder Str. 87, 81377 München",
-  description: Faker::Company.catch_phrase, 
+  description: "Don't throw away your stuff, come visit us at the Repair Cafe by Westpark! We'e got tools for fixing a wide variety of things, mainly electronics, appliances, and more! If you're looking to get something fixed or you want to learn how to do it yourself, stop on by.", 
   availability: GEN_AVAIL_MSG, 
   phone_number: Faker::PhoneNumber.cell_phone_in_e164,
 )
-space3.user = User.all.to_a.sample
+space3.user = user_boris
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Electronics").first
+space_item.space = space3
 space3.save
+
 space4 = Space.new(
   name: "WerkBox³ e.V.", 
   address: "Dachauer Str. 110c, 80636 München",
-  description: Faker::Company.catch_phrase, 
+  description: "We're all about bikes here, from mountain to road. We've got all kinds of tools to help you fix whatever problems you might have. There are also people who brings their own tools to fix other things too.",
   availability: GEN_AVAIL_MSG, 
   phone_number: Faker::PhoneNumber.cell_phone_in_e164,
 )
-space4.user = User.all.to_a.sample
+space4.user = owners.sample
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Bikes").first
+space_item.space = space4
 space4.save
+
 space5 = Space.new(
   name: "Nachbarschaftstreff am Walchenseeplatz", 
   address: "Bayrischzeller Str. 5, 81539 München",
-  description: Faker::Company.catch_phrase, 
+  description: "We run a space out of the Walchenseeplatz community center. We work mainly with clothing, but feel free to bring anything you want to fix and we'll try to help out.",
   availability: GEN_AVAIL_MSG, 
   phone_number: Faker::PhoneNumber.cell_phone_in_e164,
 )
-space5.user = User.all.to_a.sample
+space5.user = owners.sample
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Clothing").first
+space_item.space = space5
 space5.save
+
 space6 = Space.new(
   name: "Haus der Eigenarbeit – HEi", 
   address: "Wörthstraße 42, 81667 München",
-  description: Faker::Company.catch_phrase, 
+  description: "Is it broke? Don't toss it! Come here and fix it! We've got tons of tools!", 
   availability: GEN_AVAIL_MSG, 
   phone_number: Faker::PhoneNumber.cell_phone_in_e164,
 )
-space6.user = User.all.to_a.sample
+space6.user = owners.sample
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Electronics").first
+space_item.space = space6
 space6.save
+
 space7 = Space.new(
   name: "Nachbarschaftstreff Karlingerstraße", 
   address: "Karlingerstraße 30 A, 80992 München",
-  description: Faker::Company.catch_phrase, 
+  description: "Neighborhood community space set up to help local residents fix their personal belongings", 
   availability: GEN_AVAIL_MSG, 
   phone_number: Faker::PhoneNumber.cell_phone_in_e164,
 )
-space7.user = User.all.to_a.sample
+space7.user = owners.sample
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Electronics").first
+space_item.space = space7
 space7.save
 puts "** Spaces created. **"
 
 
-puts "** Attaching space images... **"
-file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943858/refective/space1_zuksfp.jpg")
-space1.photo.attach(io: file, filename: "#{space1.name}.jpg", content_type: "image/jpg")
-space1.save!
-
-
-file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943854/refective/space2_byeljp.jpg")
-space2.photo.attach(io: file, filename: "#{space2.name}.jpg", content_type: "image/jpg")
-space2.save!
-
-file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943852/refective/space3_k8xkfk.jpg")
-space3.photo.attach(io: file, filename: "#{space3.name}.jpg", content_type: "image/jpg")
-space3.save!
-
-file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943856/refective/space4_tr7fqh.jpg")
-space4.photo.attach(io: file, filename: "#{space4.name}.jpg", content_type: "image/jpg")
-space4.save!
-
-file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943842/refective/space5_rqkvog.jpg")
-space5.photo.attach(io: file, filename: "#{space5.name}.jpg", content_type: "image/jpg")
-space5.save!
-
-file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943846/refective/space6_mvh7oo.jpg")
-space6.photo.attach(io: file, filename: "#{space6.name}.jpg", content_type: "image/jpg")
-space6.save!
-
-file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943857/refective/space7_obhfea.jpg")
-space7.photo.attach(io: file, filename: "#{space7.name}.jpg", content_type: "image/jpg")
-space7.save!
-puts "** Space images attached. **"
-
-puts "** Attaching user images..."
-file = URI.open("https://kitt.lewagon.com/placeholder/users/kibex92")
-user_boris.photo.attach(io: file, filename: "#{space6.name}.jpg", content_type: "image/jpg")
-user_boris.save!
-
-file = URI.open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1609758298/wmda5l7ouqbaektxtukj.jpg")
-user_amara.photo.attach(io: file, filename: "#{space7.name}.jpg", content_type: "image/jpg")
-user_amara.save!
-
-file = URI.open("https://kitt.lewagon.com/placeholder/users/chrisrg")
-user_chris.photo.attach(io: file, filename: "#{space7.name}.jpg", content_type: "image/jpg")
-user_chris.save!
-puts "** User images attached."
 
 # Space Item Types
 puts "** Seeding space item types... **"
 spaces_list = Space.all.to_a
 Space.all.each do |space|
-  rand_num = rand(2..5)
-  item_types = ItemType.all.to_a.shuffle
+  rand_num = rand(1..3)
+  item_types = ItemType.all
+    .reject { |itemtype| itemtype.name == "Electronics" }
+    .shuffle
   rand_num.times do 
     space_item = SpaceItemType.new
     space_item.item_type = item_types.pop
@@ -193,6 +178,27 @@ Booking.all.each do |booking|
   review.user = booking.user
   review.save
 end
+review = Review.new(
+  rating: 4,
+  content: "The people were super friendly and I learned a lot! Boris runs a great space! "
+)
+review.booking = Booking.all.to_a.sample
+review.user = User.all.to_a.sample
+review.save
+review = Review.new(
+  rating: 5,
+  content: "Absolutely fantastic, Amara helped me fix an old lamp in like 10 minutes"
+)
+review.booking = Booking.all.to_a.sample
+review.user = User.all.to_a.sample
+review.save
+review = Review.new(
+  rating: 4,
+  content: "love coming here to fix small items from around the house. they have a ton of tools to use too"
+)
+review.booking = Booking.all.to_a.sample
+review.user = User.all.to_a.sample
+review.save
 puts "** Reviews created. **"
 
 # Messages
@@ -206,4 +212,49 @@ Booking.all.each do |booking|
   msg.save
 end
 puts "** Messages created. **"
+
+puts "** Attaching space images... **"
+file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943858/refective/space1_zuksfp.jpg")
+space1.photo.attach(io: file, filename: "#{space1.name}.jpg", content_type: "image/jpg")
+space1.save!
+
+
+file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1615469008/refective/space8_ltbf7r.jpg")
+space2.photo.attach(io: file, filename: "#{space2.name}.jpg", content_type: "image/jpg")
+space2.save!
+
+file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1615469008/refective/space9_suwvgd.jpg")
+space3.photo.attach(io: file, filename: "#{space3.name}.jpg", content_type: "image/jpg")
+space3.save!
+
+file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943856/refective/space4_tr7fqh.jpg")
+space4.photo.attach(io: file, filename: "#{space4.name}.jpg", content_type: "image/jpg")
+space4.save!
+
+file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943842/refective/space5_rqkvog.jpg")
+space5.photo.attach(io: file, filename: "#{space5.name}.jpg", content_type: "image/jpg")
+space5.save!
+
+file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943846/refective/space6_mvh7oo.jpg")
+space6.photo.attach(io: file, filename: "#{space6.name}.jpg", content_type: "image/jpg")
+space6.save!
+
+file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943857/refective/space7_obhfea.jpg")
+space7.photo.attach(io: file, filename: "#{space7.name}.jpg", content_type: "image/jpg")
+space7.save!
+puts "** Space images attached. **"
+
+puts "** Attaching user images..."
+file = URI.open("https://kitt.lewagon.com/placeholder/users/kibex92")
+user_boris.photo.attach(io: file, filename: "#{space6.name}.jpg", content_type: "image/jpg")
+user_boris.save!
+
+file = URI.open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1609758298/wmda5l7ouqbaektxtukj.jpg")
+user_amara.photo.attach(io: file, filename: "#{space7.name}.jpg", content_type: "image/jpg")
+user_amara.save!
+
+file = URI.open("https://kitt.lewagon.com/placeholder/users/chrisrg")
+user_chris.photo.attach(io: file, filename: "#{space7.name}.jpg", content_type: "image/jpg")
+user_chris.save!
+puts "** User images attached."
 puts "=====[ Finished seeding database ]====="

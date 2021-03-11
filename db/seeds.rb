8da7893e160ee51c1be9229bde41fb29956715dc
Chris Geekie <chris.geekie@gmail.com>
2021-03-11 16:36:45 +0100

    Final seed madness

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,7 @@ puts "** Seeding users... **"
 user_boris = User.create!(email: "boris@gmail.com", password: "password", full_name: "Boris")
 user_amara = User.create!(email: "amara@gmail.com", password: "password", full_name: "Amara")
 user_chris = User.create!(email: "chris@gmail.com", password: "password", full_name: "Chris")
+user_greta = User.create!(email: "greta@gmail.com", password: "password", full_name: "Greta")
 owners = [user_boris, user_amara]
 puts "** Users created. **"
 
@@ -36,7 +37,6 @@ puts "** Item types created. **"
 # Spaces
 puts "** Seeding spaces... **"
 GEN_AVAIL_MSG = "Mon - Fri: 5PM - 10PM, Sat: 10AM - 8PM"
-user_list = User.all.to_a
 space1 = Space.new(
   name: "Reparatur-Café Mohr-Villa", 
   address: "Situlistraße 73, 80939 München",
@@ -48,7 +48,15 @@ space1.user = owners.sample
 space_item = SpaceItemType.new
 space_item.item_type = ItemType.where(name: "Furniture").first
 space_item.space = space1
-space_item.save!
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Clothing").first
+space_item.space = space1
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Shoes").first
+space_item.space = space1
+space_item.save
 space1.save!
 
 space2 = Space.new(
@@ -61,6 +69,14 @@ space2 = Space.new(
 space2.user = owners.sample
 space_item = SpaceItemType.new
 space_item.item_type = ItemType.where(name: "Electronics").first
+space_item.space = space2
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Appliances").first
+space_item.space = space2
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Computers").first
 space_item.space = space2
 space_item.save
 space2.save
@@ -76,6 +92,15 @@ space3.user = user_boris
 space_item = SpaceItemType.new
 space_item.item_type = ItemType.where(name: "Electronics").first
 space_item.space = space3
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Appliances").first
+space_item.space = space3
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Computers").first
+space_item.space = space3
+space_item.save
 space3.save
 
 space4 = Space.new(
@@ -89,6 +114,7 @@ space4.user = owners.sample
 space_item = SpaceItemType.new
 space_item.item_type = ItemType.where(name: "Bikes").first
 space_item.space = space4
+space_item.save
 space4.save
 
 space5 = Space.new(
@@ -102,6 +128,7 @@ space5.user = owners.sample
 space_item = SpaceItemType.new
 space_item.item_type = ItemType.where(name: "Clothing").first
 space_item.space = space5
+space_item.save
 space5.save
 
 space6 = Space.new(
@@ -115,6 +142,15 @@ space6.user = owners.sample
 space_item = SpaceItemType.new
 space_item.item_type = ItemType.where(name: "Electronics").first
 space_item.space = space6
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Appliances").first
+space_item.space = space6
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Computers").first
+space_item.space = space6
+space_item.save
 space6.save
 
 space7 = Space.new(
@@ -128,32 +164,47 @@ space7.user = owners.sample
 space_item = SpaceItemType.new
 space_item.item_type = ItemType.where(name: "Electronics").first
 space_item.space = space7
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Appliances").first
+space_item.space = space7
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Computers").first
+space_item.space = space7
+space_item.save
+space_item = SpaceItemType.new
+space_item.item_type = ItemType.where(name: "Clothing").first
+space_item = SpaceItemType.new
+space_item.space = space7
+space_item.save
+space_item.item_type = ItemType.where(name: "Shoes").first
+space_item.space = space7
+space_item.save
 space7.save
 puts "** Spaces created. **"
 
-
-
 # Space Item Types
-puts "** Seeding space item types... **"
-spaces_list = Space.all.to_a
-Space.all.each do |space|
-  rand_num = rand(1..3)
-  item_types = ItemType.all
-    .reject { |itemtype| itemtype.name == "Electronics" }
-    .shuffle
-  rand_num.times do 
-    space_item = SpaceItemType.new
-    space_item.item_type = item_types.pop
-    space_item.space = space
-    space_item.save
-  end
-end
-puts "** Space item types created. **"
+# puts "** Seeding space item types... **"
+# spaces_list = Space.all.to_a
+# Space.all.each do |space|
+#   rand_num = rand(1..3)
+#   item_types = ItemType.all
+#     .reject { |itemtype| itemtype.name == "Electronics" }
+#     .shuffle
+#   rand_num.times do 
+#     space_item = SpaceItemType.new
+#     space_item.item_type = item_types.pop
+#     space_item.space = space
+#     space_item.save
+#   end
+# end
+# puts "** Space item types created. **"
 
 # Bookings
 puts "** Seeding bookings... **"
-user_list = User.all.to_a
-3.times do
+user_list = owners
+6.times do
   i_type = ItemType.all.sample
   booking = Booking.new(
     date: Faker::Date.between(from: Date.today, to: 1.month.from_now),
@@ -161,14 +212,14 @@ user_list = User.all.to_a
     comment: "Need help!",
     status: "confirmed",
   )
-  booking.user = user_list.pop
-  booking.space = Space.all.to_a.find { |space| space.user != booking.user }
+  booking.user = owners.sample
+  booking.space = Space.all.reject { |space| space == space3 }.sample
   booking.item_type = i_type
   booking.save
 end
 puts "** Bookings created. **"
 # Reviews
-puts "** Seeding reviews... **"
+puts "** Seeding random reviews... **"
 Booking.all.each do |booking|
   review = Review.new(
     rating: rand(2..5),
@@ -178,40 +229,79 @@ Booking.all.each do |booking|
   review.user = booking.user
   review.save
 end
+puts "** Random reviews created. **"
+
+puts "** Seeding fixed reviews..."
+# First fixed booking and review
+i_type = ItemType.all.sample
+booking = Booking.new( 
+  date: Faker::Date.between(from: Date.today, to: 1.month.from_now),
+  item_description: "#{i_type.name}",
+  comment: "Need help!",
+  status: "confirmed",
+)
+booking.user = user_amara
+booking.space = space3
+booking.item_type = i_type
+booking.save
 review = Review.new(
   rating: 4,
   content: "The people were super friendly and I learned a lot! Boris runs a great space! "
 )
-review.booking = Booking.all.to_a.sample
-review.user = User.all.to_a.sample
+review.booking = booking
+review.user = booking.user
 review.save
+
+# Second fixed booking and review
+i_type = ItemType.all.sample
+booking = Booking.new( date: Faker::Date.between(from: Date.today, to: 1.month.from_now),
+  item_description: "#{i_type.name}",
+  comment: "Need help!",
+  status: "confirmed",
+)
+booking.user = user_boris
+booking.space = space3
+booking.item_type = i_type
+booking.save
 review = Review.new(
   rating: 5,
   content: "Absolutely fantastic, Amara helped me fix an old lamp in like 10 minutes"
 )
-review.booking = Booking.all.to_a.sample
-review.user = User.all.to_a.sample
+review.booking = booking
+review.user = booking.user
 review.save
+
+# Third fixed booking and review
+i_type = ItemType.all.sample
+booking = Booking.new( date: Faker::Date.between(from: Date.today, to: 1.month.from_now),
+  item_description: "#{i_type.name}",
+  comment: "Need help!",
+  status: "confirmed",
+)
+booking.user = user_greta
+booking.space = space3
+booking.item_type = i_type
+booking.save
 review = Review.new(
   rating: 4,
   content: "love coming here to fix small items from around the house. they have a ton of tools to use too"
 )
-review.booking = Booking.all.to_a.sample
-review.user = User.all.to_a.sample
+review.booking = booking
+review.user = booking.user
 review.save
-puts "** Reviews created. **"
+puts "** Fixed reviews created. **"
 
-# Messages
-puts "** Seeding messages... **"
-Booking.all.each do |booking|
-  msg = Message.new(
-    content: "Thanks for making an appointment at #{booking.space.name}! We're looking forward to your visit!"
-  )
-  msg.booking = booking
-  msg.user = booking.space.user
-  msg.save
-end
-puts "** Messages created. **"
+# # Messages
+# puts "** Seeding messages... **"
+# Booking.all.each do |booking|
+#   msg = Message.new(
+#     content: "Thanks for making an appointment at #{booking.space.name}! We're looking forward to your visit!"
+#   )
+#   msg.booking = booking
+#   msg.user = booking.space.user
+#   msg.save
+# end
+# puts "** Messages created. **"
 
 puts "** Attaching space images... **"
 file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1614943858/refective/space1_zuksfp.jpg")
@@ -219,11 +309,11 @@ space1.photo.attach(io: file, filename: "#{space1.name}.jpg", content_type: "ima
 space1.save!
 
 
-file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1615469008/refective/space8_ltbf7r.jpg")
+file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1615469008/refective/space9_suwvgd.jpg")
 space2.photo.attach(io: file, filename: "#{space2.name}.jpg", content_type: "image/jpg")
 space2.save!
 
-file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1615469008/refective/space9_suwvgd.jpg")
+file = URI.open("https://res.cloudinary.com/chrisrg/image/upload/v1615469008/refective/space8_ltbf7r.jpg")
 space3.photo.attach(io: file, filename: "#{space3.name}.jpg", content_type: "image/jpg")
 space3.save!
 

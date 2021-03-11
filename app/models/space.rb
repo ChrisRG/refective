bb0b526af0ce24205901986ad1b9426454f1bf62
Chris Geekie <chris.geekie@gmail.com>
2021-03-11 09:39:25 +0100

    Added font weights to navbar, truncated space index cards, changed space cards to pills

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,7 +2,7 @@ class Space < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :space_item_types, dependent: :destroy
-  has_many :item_types, through: :space_item_types
+  has_many :item_types, -> { distinct }, through: :space_item_types
   has_many :reviews, -> { distinct }, through: :bookings
   has_one_attached :photo
   geocoded_by :address

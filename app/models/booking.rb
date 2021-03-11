32ec1209e86a4843ca0fed740e98ffe84d6c515b
Chris Geekie <chris.geekie@gmail.com>
2021-03-11 12:22:47 +0100

    Added photos to bookings and spaced booking show banner

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ApplicationRecord
   belongs_to :space
   has_many :messages, dependent: :destroy
   has_one :review, dependent: :destroy
+  has_one_attached :photo
 
   validates_presence_of :date, :item_description, :comment
 end
